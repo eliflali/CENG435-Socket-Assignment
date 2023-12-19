5ccea6d2d28d06d196fb4d256323c0202e5595c2
“elifNB” <elifNB@neuralbridge.ai>
2023-12-19 18:36:09 +0300

    udp cok tuhaf

--- ceng435-hw/code/udp/udpclient.py
+++ ceng435-hw/code/udp/udpclient.py
@@ -1,10 +1,29 @@
 import socket
+import struct
 
 def udp_client():
-    host = '172.17.0.2'
-    port = 65433
+    server_host = '172.17.0.2'  # Server IP address
+    server_port = 65433         # Server port
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        # Implement logic to send data and handle acknowledgments
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    expected_seq = 0
+    try:
+        while True:
+            packet, server_address = client_socket.recvfrom(1024)
+            seq, = struct.unpack('I', packet[:4])
+            data = packet[4:]
+
+            if seq == expected_seq:
+                print(f"Received packet: {seq}")
+                expected_seq += 1
+
+                # Send ACK with a dummy value for the second integer
+                ack_packet = struct.pack('II', seq, 0)  # Ensure this is 8 bytes
+                client_socket.sendto(ack_packet, (server_host, server_port))
+            else:
+                print(f"Out of order packet: {seq}, expected: {expected_seq}")
+    finally:
+        client_socket.close()
 
 udp_client()
--- ceng435-hw/code/udp/udpserver.py
+++ ceng435-hw/code/udp/udpserver.py
@@ -1,13 +1,61 @@
 import socket
+import struct
+import time
 
 def udp_server():
     host = '0.0.0.0'
     port = 65433
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((host, port))
-        while True:
-            data, addr = s.recvfrom(1024)
-            # Implement logic to handle received data and acknowledgments
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+
+    window_size = 1  # Initial window size
+    ssthresh = 8     # Slow start threshold
+    packets = [struct.pack('I', i) + b'Your data here' for i in range(20)]  # Example: 20 packets
+    next_seq_num = 0
+    base = 0
+    duplicate_acks = 0
+
+    try:
+        while base < len(packets):
+            while next_seq_num < base + window_size and next_seq_num < len(packets):
+                print(f"Sending packet: {next_seq_num}")
+                server_socket.sendto(packets[next_seq_num], (host, port))
+                next_seq_num += 1
+
+            server_socket.settimeout(0.5)  # Set timeout for ACKs
+            try:
+                ack_packet, _ = server_socket.recvfrom(1024)
+                if len(ack_packet) != 8:
+                    print(f"Received an incorrectly sized packet: {len(ack_packet)} bytes")
+                    continue
+
+                ack, _ = struct.unpack('II', ack_packet)
+                print(f"ACK received for packet: {ack}")
+
+                if ack > base:
+                    base = ack + 1
+                    duplicate_acks = 0
+                    if window_size < ssthresh:
+                        window_size += 1  # Slow start
+                    else:
+                        window_size += 1 / window_size  # Congestion avoidance
+                elif ack == base:
+                    duplicate_acks += 1
+                    if duplicate_acks == 3:  # Fast retransmit condition
+                        print("Triple duplicate ACKs received, fast retransmit")
+                        ssthresh = window_size / 2
+                        window_size = ssthresh + 3
+                        next_seq_num = ack + 1
+
+            except socket.timeout:
+                print(f"Timeout, resending from packet: {base}")
+                ssthresh = window_size / 2  # Multiplicative decrease
+                window_size = 1  # Reset window size
+                next_seq_num = base
+                duplicate_acks = 0
+
+    finally:
+        server_socket.close()
 
 udp_server()

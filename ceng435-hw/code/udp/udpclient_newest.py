6e4867443521a4de2c88cb8b7b76deacc8d74a97
Atakan Gelecek <88371319+atakangelecek@users.noreply.github.com>
2023-12-31 16:55:49 +0300

    Add files via upload

--- ceng435-hw/code/udp/udpclient_newest.py
+++ ceng435-hw/code/udp/udpclient_newest.py
@@ -2,12 +2,22 @@ import socket
 import struct
 import threading
 import queue
-import os
 import time
+import zlib
 
+def compute_checksum(data):
+    return zlib.crc32(data) & 0xffffffff
+
+def verify_checksum(data, received_checksum):
+    computed_checksum = compute_checksum(data)
+    return computed_checksum == received_checksum
+
+# Add a timestamp variable to track the last received packet time
+last_packet_time = time.time()
 TIMEOUT_THRESHOLD = 5  # 5 seconds timeout threshold
 
-def receive_data(client_socket, packet_queue, is_complete, last_packet_time_lock, last_packet_time):
+def receive_data(client_socket, packet_queue, server_address, is_complete, last_packet_time_lock):
+    global last_packet_time  # Use the global variable
     while not is_complete.is_set():
         try:
             packet, _ = client_socket.recvfrom(1024)
@@ -15,49 +25,89 @@ def receive_data(client_socket, packet_queue, is_complete, last_packet_time_lock
                 break
             packet_queue.put(packet)
             with last_packet_time_lock:
-                last_packet_time[0] = time.time()  # Update last packet time
+                last_packet_time = time.time()  # Update the timestamp for the last received packet
         except socket.error as e:
             print(f"Socket error: {e}")
             break
 
-def process_packets(packet_queue, received_packets, expected_seq_lock, client_socket, server_address, is_complete, last_packet_time_lock, last_packet_time):
+def process_packets(packet_queue, received_files, expected_seqs, expected_seq_lock, server_address, client_socket, is_complete, last_packet_time_lock):
+    global last_packet_time
     while not is_complete.is_set():
         try:
             packet = packet_queue.get(timeout=1)
             if not packet:
-                continue
+                break
 
-            file_index, seq = struct.unpack('II', packet[:8])
-            data = packet[8:]
+            # Unpack the file ID and sequence number
+            file_id, seq, incoming_checksum = struct.unpack('III', packet[:12])
+
+            if seq == 100000:  # Termination message
+                print("Termination message received. Ending transmission.")
+                is_complete.set()
+
+                # Write each file's data to a separate file
+                for file_id, received_packets in received_files.items():
+                    with open(f'received_file_{file_id}.obj', 'wb') as f:
+                        for i in sorted(received_packets.keys()):
+                            f.write(received_packets[i])
+                    print(f"File {file_id} reassembled and saved as 'received_file_{file_id}.obj'.")
+
+                break
+
+            data = packet[12:]
 
             with expected_seq_lock:
-                if file_index not in received_packets:
-                    received_packets[file_index] = {}
-                received_packets[file_index][seq] = data
+                if verify_checksum(data, incoming_checksum) == True:
+                    #print(f"Checksum verification successful for packet: {seq} from file {file_id}")
+                    # Initialize the dictionary for a new file ID and its expected sequence number
+                    if file_id not in received_files:
+                        received_files[file_id] = {}
+                        expected_seqs[file_id] = 0
 
-                # Send ACK for this packet
-                ack_packet = struct.pack('II', file_index, seq)
-                client_socket.sendto(ack_packet, server_address)
+                    # If we haven't received any packets for this file yet, set the expected sequence number
+                    if expected_seqs[file_id] == 0:
+                        #print("first packet received from file: ", file_id)
+                        expected_seqs[file_id] = seq
+                        expected_seq = seq 
+                    else:
+                        #print("expected seq: ", expected_seqs[file_id])
+                        expected_seq = expected_seqs[file_id]
 
-            with last_packet_time_lock:
-                current_time = time.time()
-                if current_time - last_packet_time[0] > TIMEOUT_THRESHOLD:
-                    is_complete.set()
+                    if seq == expected_seq:
+                        print(f"Received packet: {seq} from file {file_id}")
+                        received_files[file_id][seq] = data
+                        expected_seqs[file_id] += 1  # Update the expected sequence number for the file
 
+                        # Check for any buffered out-of-order packets that can now be processed
+                        while expected_seqs[file_id] in received_files[file_id]:
+                            expected_seq = expected_seqs[file_id]
+                            print(f"Received packet: {expected_seq} from file {file_id}")
+                            expected_seqs[file_id] += 1
+
+                        # Send ACK for the correctly received packet
+                        ack_packet = struct.pack('III', file_id, seq, 0)
+                        client_socket.sendto(ack_packet, server_address)
+
+                    elif seq > expected_seq:
+                        print(f"Out of order packet: {seq}, expected: {expected_seq} from file {file_id}")
+                        received_files[file_id][seq] = data
+                        # Send a NACK for the missing packet
+                        nack_packet = struct.pack('III', file_id, expected_seq, 1)
+                        client_socket.sendto(nack_packet, server_address)
+                else:
+                    #print(f"Checksum verification failed for packet: {seq} from file {file_id}")
+                    # Send a NACK for the packet with the wrong checksum
+                    nack_packet = struct.pack('III', file_id, seq, 1)
+                    client_socket.sendto(nack_packet, server_address)
         except queue.Empty:
             with last_packet_time_lock:
                 current_time = time.time()
-                if current_time - last_packet_time[0] > TIMEOUT_THRESHOLD:
-                    is_complete.set()
+                if current_time - last_packet_time > TIMEOUT_THRESHOLD:
+                    is_complete.set()  # Set the completion flag if timeout threshold is exceeded
             continue
 
-def save_received_files(received_packets, output_path):
-    for file_index in sorted(received_packets.keys()):
-        file_data = b''.join(received_packets[file_index][seq] for seq in sorted(received_packets[file_index].keys()))
-        file_path = os.path.join(output_path, f'received_file_{file_index}.obj')
-        with open(file_path, 'wb') as file:
-            file.write(file_data)
-        print(f"Saved {file_path}")
+    client_socket.close()
+
 
 def udp_client():
     server_host = "127.0.0.1"
@@ -72,23 +122,28 @@ def udp_client():
 
     packet_queue = queue.Queue()
     received_packets = {}
+    expected_seqs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
     expected_seq_lock = threading.Lock()
-    last_packet_time = [time.time()]  # Initialize last_packet_time as a list to be mutable
     last_packet_time_lock = threading.Lock()
     is_complete = threading.Event()
 
-    receiver_thread = threading.Thread(target=receive_data, args=(client_socket, packet_queue, is_complete, last_packet_time_lock, last_packet_time))
-    processor_thread = threading.Thread(target=process_packets, args=(packet_queue, received_packets, expected_seq_lock, client_socket, (server_host, server_port), is_complete, last_packet_time_lock, last_packet_time))
+    global last_packet_time
+    last_packet_time = time.time()
+
+    # Creating threads with the last_packet_time_lock passed as an argument
+    receiver_thread = threading.Thread(target=receive_data, args=(client_socket, packet_queue, (server_host, server_port), is_complete, last_packet_time_lock))
+    processor_thread = threading.Thread(target=process_packets, args=(packet_queue, received_packets, expected_seqs, expected_seq_lock, (server_host, server_port), client_socket, is_complete, last_packet_time_lock))
 
     receiver_thread.start()
     processor_thread.start()
 
-    receiver_thread.join()
-    processor_thread.join()
+    #receiver_thread.join()
+    #processor_thread.join()
+    #client_socket.close()    
 
-    save_received_files(received_packets, 'C:/Users/Administrator/Desktop/CENG435-Socket-Assignment-main/ceng435-hw/received_files')
-
-    client_socket.close()
+    # Save the received data to a file
+    
 
 if __name__ == "__main__":
     udp_client()
--- ceng435-hw/code/udp/udpserver_newest.py
+++ ceng435-hw/code/udp/udpserver_newest.py
@@ -2,97 +2,118 @@ import socket
 import struct
 import glob
 import os
+import zlib
+
+def compute_checksum(data):
+    return zlib.crc32(data) & 0xffffffff
 
 def udp_server():
     localIP = "127.0.0.1"
     localPort = 20001
     bufferSize = 1024
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     server_socket.bind((localIP, localPort))
 
-    window_size = 1
+    window_size = 4
     ssthresh = 8
     objects_path = 'C:/Users/Administrator/Desktop/CENG435-Socket-Assignment-main/ceng435-hw/objects'
 
     clientIP = "127.0.0.1"
     clientPort = 20002
 
-    obj_files = sorted(glob.glob(os.path.join(objects_path, '*.obj')))
-    #print(f"Found object files: {obj_files}")
+    file_paths = sorted(glob.glob(os.path.join(objects_path, '*.obj')))
     
-    MAX_PACKET_SIZE = 1000
-    packets_lists = {}
-    seq_number = 0
-    file_paths = []
-
-    for file_index, file_path in enumerate(obj_files):
-        file_paths.append(file_path)
-        packets_lists[file_path] = []
-        with open(file_path, 'rb') as file:
-            data = file.read()
-            chunks = [data[i:i+MAX_PACKET_SIZE] for i in range(0, len(data), MAX_PACKET_SIZE)]
-            for chunk in chunks:
-                # Include file index and sequence number in the packet
-                packet = struct.pack('II', file_index, seq_number) + chunk
-                packets_lists[file_path].append(packet)
-                #print(f"Created packet {seq_number} for file {file_path} with size {len(packet)}")
-                seq_number += 1
-    
-    # alter the order in file_paths so that 1 large and 1 small file are consecutive
     ordered_file_paths = []
     for i in range(0, len(file_paths)//2):
         ordered_file_paths.append(file_paths[i])
-        ordered_file_paths.append(file_paths[i+len(file_paths)//2])     
-   
+        ordered_file_paths.append(file_paths[i+len(file_paths)//2])
+    print(f"Found object files: {ordered_file_paths}")
+
+    #ordered_file_paths = [ordered_file_paths[1], ordered_file_paths[3]]
+
+    MAX_PACKET_SIZE = 1000
+    packets_per_file = {}
+    file_transmission_state = {}
+    sequence_number = 0
+    ack_received = 0  # Initialize an ACK counter
+    file_id = 0
+
+    for file_path in ordered_file_paths:
+        with open(file_path, 'rb') as file:
+            data = file.read()
+            chunks = [data[i:i+MAX_PACKET_SIZE] for i in range(0, len(data), MAX_PACKET_SIZE)]
+            file_packets = []
+            for chunk in chunks:
+                checksum = compute_checksum(chunk)
+                packet = struct.pack('III', file_id, sequence_number, checksum) + chunk
+                file_packets.append(packet)
+                sequence_number += 1
+
+            packets_per_file[file_id] = file_packets
+            file_transmission_state[file_id] = {'base': 0, 'next_seq_num': 0, 'window_size': window_size}
+            print(f"Created {len(file_packets)} packets for file {file_path}")
+            file_id += 1
+
+    round_robin_packet_iterators = [iter(packets) for packets in packets_per_file.values()]
+    sent_packets = 0
+
     print("Waiting for client to be ready...")
     ready_packet, address = server_socket.recvfrom(bufferSize)
     print(f"Client ready message received from {address}")
 
-    next_seq_num = 0
-    base_per_file = {file_path: 0 for file_path in ordered_file_paths}
-    acked_packets = set()
-
-    while any(base_per_file[file_path] < len(packets_lists[file_path]) for file_path in ordered_file_paths):
-        for file_path in ordered_file_paths:
-            if base_per_file[file_path] >= len(packets_lists[file_path]):
-                continue
-
-            while base_per_file[file_path] < len(packets_lists[file_path]) and next_seq_num < base_per_file[file_path] + window_size:
-                packet = packets_lists[file_path][base_per_file[file_path]]
-                print(f"Sending packet from file {file_path}, sequence number: {base_per_file[file_path]}")
-                server_socket.sendto(packet, (clientIP, clientPort))
-                next_seq_num += 1
+    try:
+        while ack_received < sequence_number:
+            for file_id, packet_iterator in enumerate(round_robin_packet_iterators):
+                state = file_transmission_state[file_id]
+                # While there's room in the window and more packets to send for this file
+                while (state['next_seq_num'] < (state['base'] + state['window_size'])) and (state['next_seq_num'] < len(packets_per_file[file_id])):
+                    try:
+                        packet_to_send = next(packet_iterator)
+                        server_socket.sendto(packet_to_send, (clientIP, clientPort))
+                        print(f"Sending packet {state['next_seq_num']} from file {file_id}")
+                        state['next_seq_num'] += 1
+                        sent_packets += 1
+                        # Break the inner loop to send packets from the next file
+                        break
+                        # BURADAKİ WINDOW SIZE IMPLEMENTATIONI MANTIKSIZ, BUNUN KONUŞULMASI LAZIM
+                    except StopIteration:
+                        # This file has no more packets to send
+                        break
 
             server_socket.settimeout(1.0)
             try:
                 ack_packet, address = server_socket.recvfrom(bufferSize)
-                if address[0] != clientIP or address[1] != clientPort:
-                    print(f"Ignoring packet from unknown address: {address}")
-                    continue
-
-                ack_file_index, ack_seq = struct.unpack('II', ack_packet[:8])
-                acked_file_path = file_paths[ack_file_index]
-                print(f"ACK received for packet: {ack_seq} from file {ack_file_index}")
-
-                # Update the base for the specific file
-                if ack_seq >= base_per_file[acked_file_path]:
-                    base_per_file[acked_file_path] = ack_seq + 1
-                    acked_packets.add((ack_file_index, ack_seq))
-
-                # Check for completion of all files
-                if all(base_per_file[file_path] == len(packets_lists[file_path]) for file_path in ordered_file_paths):
-                    print("All packets have been acknowledged. Terminating server.")
-                    server_socket.close()
-                    return
+                ack_file_id, ack_seq_num, is_nack = struct.unpack('III', ack_packet)
+                if is_nack:
+                    # This is a NACK, retransmit the specified packet
+                    packet_to_resend = packets_per_file[ack_file_id][ack_seq_num]
+                    server_socket.sendto(packet_to_resend, (clientIP, clientPort))
+                    print(f"Resending packet {ack_seq_num} from file {ack_file_id}")
+                else:
+                    state = file_transmission_state[ack_file_id]
+                    if ack_seq_num >= state['base']:
+                        state['base'] = ack_seq_num + 1
+                        ack_received += 1
+                        print(f"ACK received for packet {ack_seq_num} from file {ack_file_id}")
+                        # Removed dynamic window size adjustment here
 
             except socket.timeout:
-                print(f"Timeout, checking for missing packets...")
-                for file_path in ordered_file_paths:
-                    for seq in range(base_per_file[file_path], len(packets_lists[file_path])):
-                        if (file_paths.index(file_path), seq) not in acked_packets:
-                            print(f"Resending packet from file {file_path}, sequence number: {seq}")
-                            server_socket.sendto(packets_lists[file_path][seq], (clientIP, clientPort))
+                print("Timeout, resending packets")
+                for file_id in range(len(ordered_file_paths)):
+                    state = file_transmission_state[file_id]
+                    # Removed dynamic window size adjustment here
+                    state['next_seq_num'] = state['base']
+
+    finally:
+        #print(ack_received, sequence_number)
+        if ack_received >= sequence_number:
+            print("All packets have been acknowledged. Terminating server.")
+        else:
+            print("Server terminated without receiving all ACKs.")
+        termination_packet = struct.pack('III', 0, 100000, 0)
+        server_socket.sendto(termination_packet, (clientIP, clientPort))
+        server_socket.close()
 
 if __name__ == "__main__":
     udp_server()
